60da7340b196debdce53a64993139239c44c5015
Eric Yu <zbrachinara@gmail.com>
2022-02-16 13:59:34 -0500

    make app fullscreen on the page

--- src/app.rs
+++ src/app.rs
@@ -1,13 +1,16 @@
-use eframe::egui::CtxRef;
-use eframe::epi::Frame;
+use eframe::egui::{CtxRef, Vec2};
+use eframe::epi::{Frame};
 
 pub struct App;
 
 impl eframe::epi::App for App {
-    fn update(&mut self, ctx: &CtxRef, frame: &Frame) {
-    }
+    fn update(&mut self, ctx: &CtxRef, frame: &Frame) {}
 
     fn name(&self) -> &str {
         "Hurdle"
+    }
+
+    fn max_size_points(&self) -> Vec2 {
+        Vec2::new(10000f32, 10000f32)
     }
 }
